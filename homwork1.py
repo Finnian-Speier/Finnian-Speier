6da6896b525385780b94946c4c56788a1537b309
Finnian-Speier <86272694+Finnian-Speier@users.noreply.github.com>
2021-09-05 23:50:39 -0500

    Create homwork1.py
    my homwork one

---
+++ homwork1.py
@@ -0,0 +1,42 @@
+# 1
+x="Finnian Speier"
+print(x)
+y="I am a 21 year old collage student, who is studying economics and history"
+print(y)
+# 2
+twoToTheForth=2**4
+double=2*twoToTheForth
+print(double)
+string=str(twoToTheForth)
+doublestr=string*2
+print(doublestr)
+# 3
+escape=("This one lets you put text on a new line \n, this one prints a \
+backslash // and this one \
+prints a single quote \', finnally this one prints a double quote \" ps all the random \
+allow this one line to be split up over several lines witch is anouther escape!")
+# 4
+floAt= 1.23
+intager= 1
+print(floAt+intager)
+# this is a float
+#5
+bignum=((2**1024)+1)
+print(bignum)
+# 1797693134862315907729305190789024733617976978942306572734300811577326758055 \
+# 0096313270847732240753602112011387987139335765878976881441662249284743063947 \
+# 412437776789342486548527630221960124609411945308295208500576883815068234246288 \
+# 473913110540827237163350510684586298239947245938479716304835356329624224137217.5
+#^ thats the final result to 5
+# 6
+import string
+print (string.__dict__)
+
+#8
+z="fun"
+XandYandZ=x+y+z
+print(XandYandZ)
+print("My name is blurry face \n and i care what you \n think")
+
+
+
